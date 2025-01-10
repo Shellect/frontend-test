50c6e7a5644bbdcf862336cdfefd5f60c248008d
Your name <example@example.com>
2025-01-10 21:22:50 +0300

    add redirect to login page

--- src/app/auth.interceptor.ts
+++ src/app/auth.interceptor.ts
@@ -1,15 +1,34 @@
 import { HttpHandlerFn, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
+import { inject } from '@angular/core';
 
 export const AuthInterceptor: HttpInterceptorFn = (req: HttpRequest<any>, next: HttpHandlerFn) => {
   const idToken = localStorage.getItem("id_token");
+  const router = inject(Router);
 
   if (idToken) {
     const reqWithHeader = req.clone({
       headers: req.headers.set("Authorization", "Bearer " + idToken)
     });
 
-    return next(reqWithHeader);
+    return next(reqWithHeader)
+      .pipe(catchError(error => {
+        if (error.status === 401) {
+            console.log("TODO: сделать запрос на обновление")
+        }
+        return throwError(() => error);
+      }));
   } else {
-    return next(req);
+    return next(req)
+      .pipe(catchError(error => {
+        if (error.status === 401) {
+          
+          router.navigate(["registration"])
+        }
+        return throwError(() => error);
+      }));
   }
+
+  
 };
---
+++ src/app/auth.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private router: Router) { }
+
+  auth() {
+    this.router.navigate(["registration"])
+  }
+
+  logout() {
+    console.log("TODO: доделать выход из аккаунта")
+  }
+}
